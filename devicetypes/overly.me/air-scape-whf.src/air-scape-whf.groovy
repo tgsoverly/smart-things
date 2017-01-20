f16e942ba34d2d55563dfe121a69e1b4b73b42db
Tim <timothy@overly.me>
2017-01-19 21:18:42 -0500

    working dummy server and basic fan speed controller

--- devicetypes/overly.me/air-scape-whf.src/air-scape-whf.groovy
+++ devicetypes/overly.me/air-scape-whf.src/air-scape-whf.groovy
@@ -1,13 +1,30 @@
 /**
+* AirScap Whole House Fan Device
+*
+* Control the speed of an installed fan.  Monitor the temperature from its three sensors and it ft^/min of air flow.
+*
+* Questions:
+* 1. Timer have an upper limit?
+*
+* Todo:
+* 1. Have state turn on/off with level
+*
+* Notes:
+* 1. Command object have to return hub action
+* 1. Your hub must be selected in the device
  */
+
+preferences {
+        input("ip", "string", title:"IP Address", description: "IP of Fan", defaultValue: "10.0.1.3", required: false, displayDuringSetup: true)
+        input("port", "string", title:"Port", description: "Port of Fan", defaultValue: "80" , required: false, displayDuringSetup: true)
+}
+
 metadata {
     definition (name:"AirScape WHF", namespace:"overly.me", author:"timothy@overly.me") {
         capability "Refresh"
         capability "Switch"
         capability "Switch Level"
         capability "Polling"
-
-        attribute "levelAtOff", "number"
 
         command "levelUp"
 		command "levelDown"
@@ -25,12 +42,12 @@ metadata {
         		attributeState "default", label:'${currentValue}', backgroundColors:[
             		[value: 0, color: "#000000"],
             		[value: 1, color: "#000022"],
-            		[value: 2, color: "#000044"],
-            		[value: 3, color: "#000066"],
-            		[value: 4, color: "#000088"],
-            		[value: 5, color: "#0000aa"],
-                    [value: 6, color: "#0000cc"],
-            		[value: 7, color: "#0000ee"]
+            		[value: 2, color: "#111144"],
+            		[value: 3, color: "#111166"],
+            		[value: 4, color: "#222288"],
+            		[value: 5, color: "#2222aa"],
+                    [value: 6, color: "#3333cc"],
+            		[value: 7, color: "#3333ee"]
         		]
     		}
 
@@ -46,13 +63,21 @@ metadata {
 
 }
 
+def initialize() {
+    state.levelAtOff = 0
+}
+
 def levelUp(){
 	log.info("airscape: levelUp")
     def level = device.latestValue("level") as Integer ?: 0
+    if(level==0){
+    	sendEvent(name: "switch", value: "on")
+    }
 	if (level < 7) {
-		level = level + 1
+        level = level + 1
+        setLevel(level)
+		return getSendCodeAction(1)
 	}
-	setLevel(level)
 }
 
 def levelDown(){
@@ -60,16 +85,28 @@ def levelDown(){
     def level = device.latestValue("level") as Integer ?: 0
 	if (level > 0) {
 		level = level - 1
+        setLevel(level)
+     	if(level==0){
+        	state.levelAtOff = 1
+    		sendEvent(name: "switch", value: "off")
+    	}
+     	return getSendCodeAction(3)
 	}
-	setLevel(level)
 }
 
 def on() {
-	log.info("airscape: on")
+	log.debug("airscape: on")
+    sendEvent(name: "level", value: 1)
+    sendEvent(name: "switch", value: "on")
+    return getSendCodeAction(4)
 }
 
 def off() {
-	log.info("airscape: off")
+	log.debug("airscape: off")
+    state.levelAtOff = device.latestValue("levelAtOff") as Integer ?: 1
+    sendEvent(name: "switch", value: "off")
+    sendEvent(name: "level", value: 0)
+    return getSendCodeAction(4)
 }
 
 def setLevel(speed) {
@@ -77,16 +114,57 @@ def setLevel(speed) {
 	sendEvent(name: "level", value: speed)
 }
 
-private sendCode(code){
+def parse(description) {
+	log.debug "parse"
+    def msg = parseLanMessage(description)
+
+    def headersAsString = msg.header // => headers as a string
+    def headerMap = msg.headers      // => headers as a Map
+    def body = msg.body              // => request body as a string
+    def status = msg.status          // => http status code of the response
+    log.debug(body)
+}
+
+def poll(){
+  log.debug("polling airscape fan")
+}
+
+// gets the address of the device
+private getHostAddress() {
+    return "${ip}:${port}"
+}
+
+private setDeviceNetworkId(ip,port){
+  	def iphex = convertIPtoHex(ip)
+  	def porthex = convertPortToHex(port)
+  	device.deviceNetworkId = "$iphex:$porthex"
+  	log.debug "Device Network Id set to ${iphex}:${porthex}"
+}
+
+private String convertIPtoHex(ipAddress) {
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
+    return hex
+}
+
+private String convertPortToHex(port) {
+	String hexport = port.toString().format( '%04x', port.toInteger() )
+    return hexport
+}
+
+private getSendCodeAction(code){
   // where 1=fan speed up, 2=timer hour add, 3=fan speed down, 4=fan off
-  def result = new physicalgraph.device.HubAction(
+  log.debug("sending fan code ${code}")
+
+  //setDeviceNetworkId(ip, port)
+  def request = [
     method: "GET",
-    path: "/fanspd.cgi",
+    path: "/fanspd.cgi" + (code!=null ? "?dir=$code" : ""),
     headers: [
-        HOST: "127.0.0.1"
-    ],
-    query: [dir: code]
-  )
+        HOST: getHostAddress()
+    ]
+  ]
+
+  return new physicalgraph.device.HubAction(request)
 }
 private def title() {
     return "AirScape Whole House Fan - Copyright © 2017 Timothy Overly"
--- devicetypes/overly.me/air-scape-whf.src/server/index.js
+++ devicetypes/overly.me/air-scape-whf.src/server/index.js
@@ -10,13 +10,44 @@ http://192.168.0.20/fanspd.cgi?dir=4
 
 var express = require('express')
 var app = express()
+var xml = require('xml');
+
+var currentSpeed = 0;
+var currentTimer = 0;
 
 app.get('/fanspd.cgi', function (req, res) {
   if(req.query.dir){
-    res.send(req.query.dir);
+    console.log("dir "+req.query.dir);
+    switch (req.query.dir) {
+      case "1":
+        if(currentSpeed<7){
+          currentSpeed++;
+        }
+        break;
+      case "2":
+        currentTimer++
+        console.error("timer increased");
+        break;
+      case "3":
+        if(currentSpeed>0){
+          currentSpeed--;
+        }
+        break;
+      case "4":
+        currentSpeed = 0;
+        break;
+      default:
+        console.error("not accepted");
+    }
   }else{
-    res.send('Hello World!');
+    console.log("status return");
   }
+  if(currentSpeed==0){
+    currentTimer = 0;
+  }
+  var body = [{speed:currentSpeed}, {timer:currentTimer}, {temperature: 1000}];
+  res.set('Content-Type', 'text/xml');
+  res.send(xml({reponse: body}));
 })
 
 app.listen(3000, function () {
